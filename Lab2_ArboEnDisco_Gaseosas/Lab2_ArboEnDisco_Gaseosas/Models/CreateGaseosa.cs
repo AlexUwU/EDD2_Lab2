cd45da58a818d25be1b1cf97916c06fd585096f8
Rodrigo <Villacinda>
2020-02-23 22:53:36 -0600

    test gaseosa

--- Lab2_ArboEnDisco_Gaseosas/Lab2_ArboEnDisco_Gaseosas/Models/CreateGaseosa.cs
+++ Lab2_ArboEnDisco_Gaseosas/Lab2_ArboEnDisco_Gaseosas/Models/CreateGaseosa.cs
@@ -5,7 +5,40 @@ using System.Threading.Tasks;
 
 namespace Lab2_ArboEnDisco_Gaseosas.Models
 {
-    public class CreateGaseosa
+    public class CreateGaseosa : IGaseosasTamañoTextoFijo<Gaseosa>
     {
+
+        public Gaseosa Create(string textoTamañoFijo)
+        {
+            Gaseosa gaseosa = new Gaseosa();
+            var datos = textoTamañoFijo.Split('^');
+            gaseosa.Nombre = datos[0].Trim();
+            gaseosa.Sabor = datos[1].Trim();
+            gaseosa.CasaProductura = datos[2].Trim();
+            gaseosa.Volumen = Convert.ToInt32(datos[3].Trim());
+            gaseosa.Precio = Convert.ToInt32(datos[4].Trim());
+
+
+            return gaseosa;
+        }
+
+        public Gaseosa CreateTrim(string textoTamañoFijo)
+        {
+            Gaseosa gaseosa = new Gaseosa();
+            var datos = textoTamañoFijo.Split('^');
+            gaseosa.Nombre = datos[0].Trim('#');
+            gaseosa.Sabor = datos[1].Trim('#');
+            gaseosa.CasaProductura = datos[2].Trim('#');
+            gaseosa.Volumen = Convert.ToInt32(datos[3].Trim());
+            gaseosa.Precio = Convert.ToInt32(datos[4].Trim());
+
+            return gaseosa;
+        }
+
+        public Gaseosa CreateNulo()
+        {
+            return new Gaseosa();
+        }
+
     }
 }
